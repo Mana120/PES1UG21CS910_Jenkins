44199864766685602431c11ac59bca018bf6c794
SAI MANASA NADIMPALLI <191381679manasaab@gmail.com>
2024-03-09 20:33:06 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int main(void) {
-  cout << " rubbish"
+  cout << "Hello, Jenkins" << endl;
+  cout << "This is main.cpp made for CC Assignment 4 - Task 5" << endl;
+  
   return 0;
 }
